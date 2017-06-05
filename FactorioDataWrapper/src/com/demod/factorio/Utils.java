5cf37eb28cc0b912328838ed413f7151bd69d2df
Weston Rye (Demod) <demodude4u@gmail.com>
2017-06-04 22:41:58 -0400

    Moved or removed util methods not used here anymore.

--- FactorioDataWrapper/src/com/demod/factorio/Utils.java
+++ FactorioDataWrapper/src/com/demod/factorio/Utils.java
@@ -1,7 +1,6 @@
 package com.demod.factorio;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
@@ -10,7 +9,6 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
-import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
 import java.util.function.BiConsumer;
@@ -24,20 +22,6 @@ import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.Varargs;
 
 public final class Utils {
-	public static final BufferedImage EMPTY_IMAGE = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-	static {
-		EMPTY_IMAGE.setRGB(0, 0, 0x00000000);
-	}
-
-	public static int compareRange(double min1, double max1, double min2, double max2) {
-		if (max1 <= min2) {
-			return -1;
-		}
-		if (max2 <= min1) {
-			return 1;
-		}
-		return 0;
-	}
 
 	public static void debugPrintJson(JSONArray json) {
 		debugPrintJson("", json);
@@ -130,20 +114,6 @@ public final class Utils {
 		}
 	}
 
-	public static Color getAverageColor(BufferedImage image) {
-		int[] pixels = image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
-		float sumR = 0, sumG = 0, sumB = 0, sumA = 0;
-		for (int pixel : pixels) {
-			float a = (pixel >> 24) & 0xFF;
-			float f = a / 255;
-			sumA += a;
-			sumR += ((pixel >> 16) & 0xFF) * f;
-			sumG += ((pixel >> 8) & 0xFF) * f;
-			sumB += ((pixel) & 0xFF) * f;
-		}
-		return new Color(sumR / sumA, sumG / sumA, sumB / sumA);
-	}
-
 	public static Color parseColor(LuaValue value) {
 		float red = value.get("r").tofloat();
 		float green = value.get("g").tofloat();
@@ -182,53 +152,6 @@ public final class Utils {
 		return new JSONObject(new Scanner(in, "UTF-8").useDelimiter("\\A").next());
 	}
 
-	public static BufferedImage scaleImage(BufferedImage image, int width, int height) {
-		BufferedImage ret = new BufferedImage(width, height, image.getType());
-		Graphics2D g = ret.createGraphics();
-		g.drawImage(image, 0, 0, width, height, null);
-		g.dispose();
-		return ret;
-	}
-
-	public static <T> void sortWithNonTransitiveComparator(T[] array, Comparator<T> comparator) {
-		@SuppressWarnings("unchecked")
-		T[] tmp = (T[]) new Object[array.length];
-		sortWithNonTransitiveComparator_MergeSort(array, comparator, tmp, 0, array.length - 1);
-	}
-
-	private static <T> void sortWithNonTransitiveComparator_Merge(T[] a, Comparator<T> comparator, T[] tmp, int left,
-			int right, int rightEnd) {
-		int leftEnd = right - 1;
-		int k = left;
-		int num = rightEnd - left + 1;
-
-		while (left <= leftEnd && right <= rightEnd)
-			if (comparator.compare(a[left], a[right]) <= 0)
-				tmp[k++] = a[left++];
-			else
-				tmp[k++] = a[right++];
-
-		while (left <= leftEnd) // Copy rest of first half
-			tmp[k++] = a[left++];
-
-		while (right <= rightEnd) // Copy rest of right half
-			tmp[k++] = a[right++];
-
-		// Copy tmp back
-		for (int i = 0; i < num; i++, rightEnd--)
-			a[rightEnd] = tmp[rightEnd];
-	}
-
-	private static <T> void sortWithNonTransitiveComparator_MergeSort(T[] a, Comparator<T> comparator, T[] tmp,
-			int left, int right) {
-		if (left < right) {
-			int center = (left + right) / 2;
-			sortWithNonTransitiveComparator_MergeSort(a, comparator, tmp, left, center);
-			sortWithNonTransitiveComparator_MergeSort(a, comparator, tmp, center + 1, right);
-			sortWithNonTransitiveComparator_Merge(a, comparator, tmp, left, center + 1, right);
-		}
-	}
-
 	public static void terribleHackToHaveOrderedJSONObject(JSONObject json) {
 		try {
 			Field map = json.getClass().getDeclaredField("map");
@@ -259,10 +182,6 @@ public final class Utils {
 		}
 		ret.setRGB(0, 0, w, h, pixels, 0, w);
 		return ret;
-	}
-
-	public static Color withAlpha(Color color, int alpha) {
-		return new Color(color.getRed(), color.getGreen(), color.getBlue(), alpha);
 	}
 
 	private Utils() {
