14d25da71412cb75d114d0c93b42964bf8015db4
Weston Rye (Demod) <demodude4u@gmail.com>
2017-06-21 13:30:58 -0400

    Apparently some fields are optional...

--- FactorioDataWrapper/src/com/demod/factorio/ModInfo.java
+++ FactorioDataWrapper/src/com/demod/factorio/ModInfo.java
@@ -20,11 +20,11 @@ public class ModInfo {
 	public ModInfo(JSONObject json) {
 		name = json.getString("name");
 		version = json.getString("version");
-		title = json.getString("title");
-		author = json.getString("author");
-		contact = json.getString("contact");
-		homepage = json.getString("homepage");
-		description = json.getString("description");
+		title = json.optString("title", "");
+		author = json.optString("author", "");
+		contact = json.optString("contact", "");
+		homepage = json.optString("homepage", "");
+		description = json.optString("description", "");
 		JSONArray dependenciesJson = json.getJSONArray("dependencies");
 		for (int i = 0; i < dependenciesJson.length(); i++) {
 			dependencies.add(dependenciesJson.getString(i));
