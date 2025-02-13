699f3ce83596b2954f1cca8fb1df1b536a754b8d
Demod <demodulent@gmail.com>
2025-02-13 01:21:18 -0500

    tile foundation

--- FactorioDataWrapper/src/com/demod/factorio/prototype/TilePrototype.java
+++ FactorioDataWrapper/src/com/demod/factorio/prototype/TilePrototype.java
@@ -3,7 +3,15 @@ package com.demod.factorio.prototype;
 import com.demod.factorio.fakelua.LuaTable;
 
 public class TilePrototype extends DataPrototype {
+	private final boolean foundation;
+
 	public TilePrototype(LuaTable lua, String name, String type) {
 		super(lua, name, type);
+
+		foundation = lua.get("is_foundation").optboolean(false);
+	}
+
+	public boolean isFoundation() {
+		return foundation;
 	}
 }
